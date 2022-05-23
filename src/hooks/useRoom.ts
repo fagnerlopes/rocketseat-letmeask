714ccc279dc82129fb3e4974ac1f0fc61c1f6798
Fagner Lopes <fagner.lopes@kinghost.com.br>
2022-05-23 17:00:20 -0300

    [OK] Implements like method

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     id: string;
@@ -9,9 +10,11 @@ type Question = {
     email: string;
     avatar: string;
   };
+  likeCount: number;
   content: string;
   isHighlighted: boolean;
   isAnswered: boolean;
+  likeId: string | undefined;
 };
 
 type FirebaseQuestions = Record<
@@ -23,6 +26,12 @@ type FirebaseQuestions = Record<
       email: string;
       avatar: string;
     };
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
     content: string;
     isHighlighted: boolean;
     isAnswered: boolean;
@@ -30,10 +39,11 @@ type FirebaseQuestions = Record<
 >;
 
 export function useRoom(roomId: string) {
+  const { user } = useAuth();
   if (!roomId) {
     throw new Error("Room ID don't can be empty or null");
   }
-  const [questionItems, setQuestionItems] = useState<Question[]>([]);
+  const [questionItems, setQuestionItems] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState<string>("");
   const [authorId, setAuthorId] = useState<string>("");
 
@@ -52,14 +62,22 @@ export function useRoom(roomId: string) {
             author: value.author,
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0],
           };
         }
       );
       setAuthorId(databaseRoom.authorId);
       setTitle(databaseRoom.title);
       setQuestionItems(parseQuestions);
+
+      return () => {
+        roomRef.off("value");
+      };
     });
-  }, [roomId]);
+  }, [roomId, user?.id]);
 
   return { questionItems, title, authorId };
 }
